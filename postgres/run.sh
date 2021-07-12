246fd1685b25a05d9667b4e9e966c5e0263321e7
chrisjsimpson <chris.j.simpson@live.co.uk>
2021-07-12 21:52:33 +0100

    wip #14 postgres/run.sh remove container when stopped
    name postgres container unambiguously
    
    run postgress container to backgroun
    
    document how to access postgress logs (since its running in the background by default

--- postgres/run.sh
+++ postgres/run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+docker stop postgres-salon-booking-guru > /dev/null | true
 docker run -it \
-    --name sbg-postgres \
+    --rm \
+    -d \
+    --name postgres-salon-booking-guru \
     -e POSTGRES_PASSWORD="veryinsecure" \
     -e POSTGRES_DB="postgres" \
     -e PGDATA=/var/lib/postgresql/data/pgdata \
-    -v sbg-postgres:/var/lib/postgresql/data \
+    -v postgres:/var/lib/postgresql/data \
     -p 5432:5432 \
     postgres
