a4371f0ab6d1cba77d5332e31e240b19b9d5a563
chrisjsimpson <chris.j.simpson@live.co.uk>
2021-08-01 22:45:48 +0100

    test #5 pr

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="container mx-auto px-6 pt-8">
-        <div class="mb-4">Welcome home!</div>
-        <Button>PrimeVue Button!</Button>
-    </div>
+  <div class="container mx-auto px-6 pt-8">
+    <div class="mb-4">Home</div>
+    <Button>PrimeVue Button!</Button>
+  </div>
 </template>
 
 <script lang="ts">
 // vue
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({});
 </script>
