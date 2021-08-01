29b93ac7e86a8cb845cd3eaa5cc8549563d9f09d
Jamie Scollay <jamiescollay@hotmail.com>
2021-08-01 14:58:15 +0100

    Fix #116 API will now run even if there is no database connection present

--- api/test.sh
+++ api/test.sh
@@ -3,5 +3,5 @@
 SALON_BOOKING_GURU_DB_HOST="localhost" \
 SALON_BOOKING_GURU_DB_PORT="5432" \
 SALON_BOOKING_GURU_DB_USER="postgres" \
-SALON_BOOKING_GURU_DB_PASSWORD="veryinsecure" \
+SALON_BOOKING_GURU_DB_PASSWORD="changeme" \
 go test -v -cover ./... | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
