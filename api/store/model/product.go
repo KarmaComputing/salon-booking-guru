f76d77ec93ce6f571a6e4ee0ab9b86f5b00e9dd0
Jamie Scollay <jamiescollay@hotmail.com>
2021-09-18 01:09:40 +0100

    Fix #47 Implemented product model

--- api/store/model/product.go
+++ api/store/model/product.go
@@ -1,8 +1,11 @@
 package model
 
 type Product struct {
-	Id       int     `json:"id"`
-	Name     string  `json:"name"`
-	Price    float64 `json:"price"`
-	Currency string  `json:"currency"`
+	Id                int     `json:"id"`
+	ProductCategoryId int     `json:"productCategoryId"`
+	Name              string  `json:"name"`
+	Description       string  `json:"description"`
+	Price             float64 `json:"price"`
+	Deposit           float64 `json:"deposit"`
+	Duration          float64 `json:"duration"`
 }
